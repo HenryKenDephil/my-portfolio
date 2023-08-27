582d03a86c2c88814b4cec31450300496d69f62b
HenryKenDePhil <h.komoloh@gmail.com>
2023-08-27 19:22:24 +0300

    created header  content and linked the contact page to home page

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 
 import Home from './containers/home';
 import About from './containers/about';
@@ -9,26 +11,43 @@ import Portfolio from './containers/portfolio';
 import Contact from './containers/contact';
 import Navbar from './components/navBar';
 
+import particles from "./utils.js/particles";
 function App() {
+
+  const location = useLocation();
+  console.log(location);
+
+  const handleInit = async (main) =>{
+    await loadFull(main)
+  }
+
+  const renderParticleJsInHomePage = location.pathname === '/';
   return (
     <div className="App">
       {/* particles */}
-
+      {
+        renderParticleJsInHomePage && (
+          <Particles id="particles" options={particles} init={handleInit}/> 
+        )
+      }
+       
       {/* navar */}
       <Navbar/>
 
       {/* main page content */}
 
+      <div className='App__main-page-content'>
       <Routes>
         
-          <Route index path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/resume' element={<Resume />} />
-          <Route path='/skills' element={<Skills />} />
-          <Route path='/portfolio' element={<Portfolio />} />
-          <Route path='/contact' element={<Contact />} />
-        
-      </Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      
+    </Routes>
+      </div>
     </div>
   );
 }
--- portfolio_website/src/containers/home/index.js
+++ portfolio_website/src/containers/home/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import { Animate } from 'react-simple-animate';
+import './styles.scss';
 const Home = () => {
-    return ( 
-        <div>
-            Home Page
-        </div>
-     );
+
+    const navigate = useNavigate();
+    console.log(navigate)
+
+    const handleNavigateToContactMePage = () => {
+        navigate("/contact");
+    };
+    return (
+        <section id="home">
+            <div className="home__text-wrapper">
+                <h1>
+                    Hello, I'm Henry Kennedy,
+                    <br />
+                    A Software & Security
+                    <br />
+                    Engineer
+
+                </h1>
+            </div>
+            <Animate
+            play
+            duration={1.5}
+            display={1}
+            start={{
+                transform: 'translateY(550px)'
+            }}
+
+            end={{
+                transform: 'translateX(0px)'
+            }}
+            >
+                <div className="home__contact-me">
+                    <button onClick={handleNavigateToContactMePage}>Open to Work, Contact Me</button>
+                </div>
+            </Animate>
+
+        </section>
+    );
 }
- 
+
 export default Home;
